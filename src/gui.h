d27a96f4473cf31e73163fe4c91ce3927de4e345
Szymon120000 <jjsjyckk@gmail.com>
2026-04-28 09:41:35 -0700

    Add proxy makefiles so make[1]: Entering directory '/workspace/os' mkdir -p build/esp/EFI/BOOT cp build/BOOTX64.EFI build/esp/EFI/BOOT/BOOTX64.EFI dd if=/dev/zero of=build/efiboot.img bs=1M count=64 status=none mkfs.fat -F 32 build/efiboot.img make[1]: Leaving directory '/workspace/os' resolves reliably in WSL/Linux

---
+++ src/gui.h
@@ -0,0 +1,10 @@
+#ifndef NEBULA_GUI_H
+#define NEBULA_GUI_H
+
+#include "gfx.h"
+#include "exu.h"
+
+void gui_render_desktop(Gfx *gfx, int installed);
+void gui_launch_app(Gfx *gfx, const ExuImage *app, UINT32 slot);
+
+#endif
